52f498522bf554c242de57b700067bfb71d61946
Mohamed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-07-10 11:13:54 +0200

    session 15 add angular-in-memory-web-api addon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { FriendsComponent } from './friends/friends.component';
 import { routing, appRoutingProviders } from './app.routing';
 import { FriendComponent } from './friend/friend.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { MockDatabaseService } from './mock.database.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,10 @@ import { FriendComponent } from './friend/friend.component';
     BrowserModule,
     FormsModule,
     routing,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(MockDatabaseService, {
+      delay: 100, rootPath: 'api/'
+    })
   ],
   providers: [UserService, FeedService, appRoutingProviders],
   bootstrap: [AppComponent]
--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -8,12 +8,7 @@ import { Tweet } from './tweet';
 export class FeedService {
 
   tweets = [
-    new Tweet('Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', 'Glen', new Date(), ['Joe'], []),
-    new Tweet('Measuring programming progress by lines of code is like measuring aircraft building progress by weight', 'Joe', new Date(), [], ['Mary']),
-    new Tweet('Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 'Mary', new Date(), ['Glen'], ['Mary']),
-    new Tweet('People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on each other, like a wall of mini stones', 'Glen', new Date(), ['Joe', 'Mary'], []),
-    new Tweet('You can’t have great software without a great team, and most software teams behave like dysfunctional families.', 'Joe', new Date(), [], ['Mary', 'Glen'])
-  ];
+    ];
 
   constructor(private userService: UserService, private http: Http) { }
 
@@ -23,7 +18,7 @@ export class FeedService {
 
   postNewTweet(tweetText: string) {
     this.tweets.unshift(
-      new Tweet(tweetText, this.userService.getCurrentUser(), new Date(), [], [])
+      new Tweet(5,tweetText, this.userService.getCurrentUser(), new Date(), [], [])
     );
   }
 
